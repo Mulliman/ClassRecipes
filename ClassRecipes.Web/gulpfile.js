cefd9a2abd6785acacf0ea4dee38d78e309bec40
Sam Mullins <sammullins.co.uk@gmail.com>
2019-01-24 18:30:28 +0000

    Added gulp for SCSS support

---
+++ ClassRecipes.Web/gulpfile.js
@@ -0,0 +1,16 @@
+﻿var gulp = require('gulp');
+
+var sass = require("gulp-sass");
+var paths = {
+    webroot: "./wwwroot/" /*Set this to your path accordingly*/
+};
+
+paths.scss = paths.webroot + "scss/**/*.scss"; /*watches sub folders inside wwwroot/css/ */
+gulp.task('sass', function () {
+    gulp.src(paths.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.webroot + "css"));
+});
+gulp.task('watch-sass', function () {
+    gulp.watch(paths.scss, ['sass']);
+})
