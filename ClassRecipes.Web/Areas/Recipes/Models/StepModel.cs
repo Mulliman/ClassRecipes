c65604faca7060c685fc481bf69a6407f4b97ede
Sam Mullins <sammullins.co.uk@gmail.com>
2019-01-25 22:31:22 +0000

    Recipe steps now output on the page.

---
+++ ClassRecipes.Web/Areas/Recipes/Models/StepModel.cs
@@ -0,0 +1,52 @@
+﻿using ClassRecipes.Core;
+using ClassRecipes.Web.Areas.Common.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassRecipes.Web.Areas.Recipes.Models
+{
+    public class StepModel
+    {
+        public StepModel(Step step, int index)
+        {
+            Step = step;
+            Index = index;
+        }
+
+        public Step Step { get; }
+
+        public int Index { get; }
+
+        public int NextStepIndex => Index + 1;
+
+        public IEnumerable<CollectionItemModel> ToolItems => Step?.Tools?.Select(i => new CollectionItemModel(i.GetCorrectSingularOrPlural(), i.GetQuantityLabel()));
+
+        public IEnumerable<CollectionItemModel> IngredientItems => Step?.Ingredients?.Select(i => new CollectionItemModel(i.GetCorrectSingularOrPlural(), i.GetQuantityLabel()));
+
+        public IEnumerable<CollectionItemModel> ToolsAndIngredients
+        {
+            get
+            {
+                var list = new List<CollectionItemModel>();
+
+                if(ToolItems?.Any() == true)
+                {
+                    list.AddRange(ToolItems);
+                }
+
+                if (IngredientItems?.Any() == true)
+                {
+                    list.AddRange(IngredientItems);
+                }
+                
+                return list;
+            }
+        }
+
+        public CollectionWithHeaderModel ToolsAndIngredientsCollection => new CollectionWithHeaderModel
+        {
+            Header = "Step " + Index,
+            Items = ToolsAndIngredients
+        };
+    }
+}
