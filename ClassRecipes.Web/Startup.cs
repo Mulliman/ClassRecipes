077eeb3cfe49131ce199e1a2b3b8a6aad6e2a936
Sam Mullins <sammullins.co.uk@gmail.com>
2019-01-25 21:04:32 +0000

    Now works on IIS.

--- ClassRecipes.Web/Startup.cs
+++ ClassRecipes.Web/Startup.cs
@@ -33,10 +33,9 @@ namespace ClassRecipes.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
 
             app.UseMvc(routes =>
             {
