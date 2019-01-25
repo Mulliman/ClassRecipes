72b965da650528fcbac17ae84eb4f89817e1356a
Sam Mullins <sammullins.co.uk@gmail.com>
2019-01-25 20:38:02 +0000

    Theming structure

--- ClassRecipes.Web/gulpfile.js
+++ ClassRecipes.Web/gulpfile.js
@@ -1,15 +1,34 @@
 ﻿var gulp = require('gulp');
+var concat = require('gulp-concat');
 
 var sass = require("gulp-sass");
 var paths = {
-    webroot: "./wwwroot/" /*Set this to your path accordingly*/
+    webroot: "./wwwroot/",
+    common: "./Areas/Common/",
+    recipes: "./Areas/Recipes/",
 };
 
-paths.scss = paths.webroot + "scss/**/*.scss"; /*watches sub folders inside wwwroot/css/ */
+paths.baseScss = paths.common + "scss/base.scss";
+paths.recipesBaseScss = paths.recipes + "scss/Base/recipe-base.scss";
+paths.recipesThemesScss = paths.recipes + "scss/Themes/*.scss";
+
 gulp.task('sass', function () {
-    gulp.src(paths.scss)
+
+    gulp.src(paths.baseScss)
+        .pipe(sass())
+        .pipe(concat('base.css'))
+        .pipe(gulp.dest(paths.webroot + "css"));
+
+    gulp.src(paths.recipesBaseScss)
+        .pipe(sass())
+        .pipe(concat('recipe-base.css'))
+        .pipe(gulp.dest(paths.webroot + "css"));
+
+    gulp.src(paths.recipesThemesScss)
         .pipe(sass())
         .pipe(gulp.dest(paths.webroot + "css"));
+
+    
 });
 gulp.task('watch-sass', function () {
     gulp.watch(paths.scss, ['sass']);
