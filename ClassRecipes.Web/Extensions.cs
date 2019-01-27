5b0ac22eb78c60c05e324416d32a3968db2532d6
Sam Mullins <sammullins.co.uk@gmail.com>
2019-01-27 14:57:13 +0000

    TimeSpan formatting for recipes.

---
+++ ClassRecipes.Web/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassRecipes.Web
+{
+    public static class Extensions
+    {
+        public static string ToReadableTimeSpan(this TimeSpan span)
+        {
+            string formatted = string.Format("{0}{1}{2}",
+                span.Duration().Days > 0 ? string.Format("{0:0}d ", span.Days) : string.Empty,
+                span.Duration().Hours > 0 ? string.Format("{0:0}h ", span.Hours) : string.Empty,
+                span.Duration().Minutes > 0 ? string.Format("{0:0}m", span.Minutes) : string.Empty);
+            
+            return formatted.Trim();
+        }
+    }
+}
